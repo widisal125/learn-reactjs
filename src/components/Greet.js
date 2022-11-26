676be60518c4ff20a5d0e9a77c86ace1933ecb4c
Shan <shanchathuranga@gmail.com>
2022-11-26 07:24:38 +0530

    Greet arrow function

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-function Greet() {
-  return <h1>Hello Shan</h1>
-}
+// function Greet() {
+//   return <h1>Hello Shan</h1>
+// }
+
+const Greet = () => <h1>Hello Shan</h1>
 
 export default Greet;
